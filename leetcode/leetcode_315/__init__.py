05b3b84ad22bb828da03993ecb44dda7422077dc
lin.liu <2686221966@qq.com>
2023-07-08 20:59:49 +0800

    LeetCode 167. 两数之和 II - 输入有序数组
    LeetCode 167. 两数之和 II - 输入有序数组

---
+++ leetcode/leetcode_315/__init__.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            sum = numbers[i] + numbers[j]
+            if sum > target:
+                j -= 1
+            elif sum < target:
+                i += 1
+            else:
+                return [i, j]
+        return [-1, -1]
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum([2, 7, 11, 15], 9))
